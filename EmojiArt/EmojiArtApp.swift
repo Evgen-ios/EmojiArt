6820e43ac6106a71d0239686eb12cfe395326492
Evgen-ios <jack38irk@icloud.com>
2020-12-09 11:43:53 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Evgeniy Goncharov on 09.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
